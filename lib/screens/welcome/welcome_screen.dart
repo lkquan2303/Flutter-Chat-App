0866da4a29918c3ef8c16778467bf1693b3516bd
Quan Le <quan.le@digitalfortress.com>
2021-06-03 23:26:38 +0700

    feat: Init Source and Implement Welcome source

---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset('assets/images/welcome_image.png'),
+            Text(
+              'Welcome to our freedom \nmessaging app',
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Freedom talk any ferson of your \nmother language',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .color
+                    .withOpacity(0.64),
+              ),
+            ),
+            FittedBox(
+              child: TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Text(
+                      'Skip',
+                      style: Theme.of(context).textTheme.bodyText1.copyWith(
+                            color: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                .color
+                                .withOpacity(0.8),
+                          ),
+                    ),
+                    Icon(Icons.arrow_forward_ios,
+                        size: 16,
+                        color: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            .color
+                            .withOpacity(0.8))
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
